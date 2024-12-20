5b77c4a0cfc8c1b26c0a1b8b1c38b3db51e3d6d0
writemorecode <gustav.ae.karlsson@gmail.com>
2024-12-20 22:53:35 +0100

    Initial Commit

---
+++ GroceryTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryTracker
+//
+//  Created by Gustav Karlsson on 2024-12-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryTracker/GroceryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryTrackerApp.swift
+//  GroceryTracker
+//
+//  Created by Gustav Karlsson on 2024-12-20.
+//
+
+import SwiftUI
+
+@main
+struct GroceryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
