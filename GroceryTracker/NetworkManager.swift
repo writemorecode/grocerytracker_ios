fe1af8c568608fbceb4ce0ae3cd9defe2151531f
writemorecode <gustav.ae.karlsson@gmail.com>
2025-01-26 23:29:22 +0100

    Reduced code duplication in NetworkManager

--- GroceryTracker/NetworkManager.swift
+++ GroceryTracker/NetworkManager.swift
@@ -19,8 +19,8 @@ class NetworkManager {
     static let shared = NetworkManager()
     private let baseURL = "http://192.168.0.6:3000";
     
-    func uploadProduct(_ product: ProductData) async throws {
-        guard let url = URL(string: "\(baseURL)/products") else {
+    private func performPostRequest<T: Encodable>(endpoint: String, body: T) async throws -> Data {
+        guard let url = URL(string: "\(baseURL)\(endpoint)") else {
             throw NetworkError.invalidURL
         }
         
@@ -29,35 +29,7 @@ class NetworkManager {
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
         let encoder = JSONEncoder()
-        let jsonData = try encoder.encode(product)
-        request.httpBody = jsonData
-        
-        let (_, response) = try await URLSession.shared.data(for: request)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw NetworkError.invalidResponse
-        }
-        
-        guard (200...299).contains(httpResponse.statusCode) else {
-            throw NetworkError.httpError(httpResponse.statusCode)
-        }
-    }
-    
-    struct StoreResponse: Decodable {
-        let id: Int
-    }
-
-    func uploadStore(_ store: StoreRecord) async throws -> Int {
-        guard let url = URL(string: "\(baseURL)/stores") else {
-            throw NetworkError.invalidURL
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let encoder = JSONEncoder()
-        let jsonData = try encoder.encode(store)
+        let jsonData = try encoder.encode(body)
         request.httpBody = jsonData
         
         let (data, response) = try await URLSession.shared.data(for: request)
@@ -70,6 +42,19 @@ class NetworkManager {
             throw NetworkError.httpError(httpResponse.statusCode)
         }
         
+        return data
+    }
+    
+    func uploadProduct(_ product: ProductData) async throws {
+        _ = try await performPostRequest(endpoint: "/products", body: product)
+    }
+    
+    struct StoreResponse: Decodable {
+        let id: Int
+    }
+    
+    func uploadStore(_ store: StoreRecord) async throws -> Int {
+        let data = try await performPostRequest(endpoint: "/stores", body: store)
         let decoder = JSONDecoder()
         let storeResponse = try decoder.decode(StoreResponse.self, from: data)
         return storeResponse.id
